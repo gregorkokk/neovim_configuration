95e1ab54fd0e87ce8b8354b5f344c46ab35f6cc8
gregor kokk <kokkgregor@gmail.com>
2025-05-21 14:35:03 +0300

    add autopairs.lua, sets up automatic insertion of closing pairs for example, type ( it automatically adds )

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,36 @@
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
+    local npairs = require("nvim-autopairs")
+
+    npairs.setup({
+      check_ts = true, -- Use treesitter if available
+      ts_config = {
+        lua = {'string'},
+        python = {'string'},
+        cpp = {'string'},
+      },
+      -- Don't add pairs if the next character is alphanumeric
+      ignored_next_char = "[%w%.]",
+      fast_wrap = {
+        map = "<leader>e",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        highlight = "Search",
+        highlight_grey = "Comment"
+      },
+    })
+
+    -- Integration with nvim-cmp
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    local cmp = require('cmp')
+    cmp.event:on(
+      'confirm_done',
+      cmp_autopairs.on_confirm_done()
+    )
+  end,
+}
