3899c344e2fecc987d2db1bf99293b278398ff0f
gregor kokk <kokkgregor@gmail.com>
2025-07-15 11:51:40 +0300

    can change to different catppuccin themes

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,13 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme "catppuccin-mocha"
+    -- Light theme
+    --vim.cmd.colorscheme "catppuccin-latte"
+    -- Cool dark
+    -- vim.cmd.colorscheme "catppuccin-frappe"
+    -- Warm dark
+    vim.cmd.colorscheme "catppuccin-macchiato"
+    -- Darkest -> current
+    -- vim.cmd.colorscheme "catppuccin-mocha"
   end
 }
